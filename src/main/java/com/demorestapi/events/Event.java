4a4e446ea174585e7b0ea06106e89431d9d86807
youngG209 <young209@mcocoa.com>
2021-03-08 11:58:07 +0900

    Event 도메인 구현

---
+++ src/main/java/com/demorestapi/events/Event.java
@@ -0,0 +1,26 @@
+package com.demorestapi.events;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @NoArgsConstructor @AllArgsConstructor
+public class Event {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location; //이게없으면 온라인 모임
+    private int basePrice;
+    private int maxPrice;
+    private int limitOfEnrollment;
+    private boolean offline;
+    private boolean free;
+    private EventStatus eventStatus;
+
+}
---
+++ src/test/java/com/demorestapi/events/EventTest.java
@@ -0,0 +1,32 @@
+package com.demorestapi.events;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Spring REST API")
+                .description("REST API development with Spring")
+                .build();
+
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+        String name = "Event";
+        String description = "String";
+
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+
+    }
+}
